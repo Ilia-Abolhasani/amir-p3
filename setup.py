d451904ac2e9b1dec750d48c811b03add1e7c0a7
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2022-04-28 21:48:50 +0430

    setup modified.

--- setup.py
+++ setup.py
@@ -1,29 +1,17 @@
-import sys
 from setuptools import setup, find_packages
-from setuptools import Extension
-import os
 
-SETUP_METADATA = \
-               {
+SETUP_METADATA = {
     "name": "ct-analizer",
+    "version": "1.0.0",
     "description": "",
     "url": "https://github.com/",
     "author": "",
-    "author_email": "",
-    "version": "1.0.0",
+    "author_email": "",    
     "license": "MIT",
-    "packages": find_packages(),
-    "entry_points": {'console_scripts': [
-        'vamb = vamb.__main__:main'
-        ]
-    },
-    "scripts": ['src/concatenate.py'],
-    "ext_modules": [Extension("vamb._vambtools",
-                               sources=["src/_vambtools.pyx"],
-                               language="c")],
-    "install_requires": ["numpy~=1.20", "torch~=1.8", "pysam~=0.14"],
-    "setup_requires": ['Cython~=0.29', "setuptools~=58.0"],
-    "python_requires": "~=3.7",
+    "packages": find_packages(where="src"),            
+    "install_requires": ["numpy~=1.20"],
+    "setup_requires": ["setuptools~=58.0"],
+    "python_requires": ">=3.6",
     "classifiers":[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
