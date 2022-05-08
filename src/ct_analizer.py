362a243112031b508ef1997519eaa9764bb39c26
emadi <Abolhasani.eliya@gmail.com>
2022-05-08 21:02:02 +0430

    ct_analizer imported to main pipline

--- src/ct_analizer.py
+++ src/ct_analizer.py
@@ -4,6 +4,12 @@ import os, sys
 import numpy as np
 import pandas as pd
 from subprocess import Popen, PIPE, STDOUT
+import pandas as pd
+if sys.version_info[0] < 3: 
+    from StringIO import StringIO
+else:
+    from io import StringIO
+    
 
 
 class dotdict(dict):    
@@ -11,13 +17,147 @@ class dotdict(dict):
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
-
-with open('./src/Title.json') as json_file:
+with open('./src/config/titles.json') as json_file:
     titles = dotdict(json.load(json_file))
 
 
-with open('./src/Error.json') as json_file:
+with open('./src/config/errors.json') as json_file:
     errors = dotdict(json.load(json_file))
+
+    
+def adjust(text, n=7):
+    text = str(text)
+    return " " * (n - len(text)) + text
+
+
+def bracket_row(row):    
+    s = row['data']
+    index = min(s.find('.'), s.find('('))
+    data = row['data']
+    row['data'] = data[0:index]
+    row['bracket'] = data[index:]
+    return row
+
+
+def bracket_to_ct(tag, data, bracket, deltaG, negative_deltaG=True):
+    deltaG = deltaG.replace('(','').replace(')','')
+    deltaG = float(deltaG)
+    if(deltaG > 0 and negative_deltaG ): # negetive?!
+        deltaG = -1 * deltaG
+    stack = []
+    index = np.zeros((len(bracket)), dtype = int)
+    values = np.zeros((len(bracket)), dtype = int)
+    for i in range(len(bracket)):
+        index[i] = i + 1
+        if(bracket[i] == '.'):
+            values[i] = 0
+        elif(bracket[i] == '('):
+            stack.append(i)
+        elif(bracket[i] == ')'):
+            if(len(stack) == 0 ):
+                print('structure error!')
+            values[stack[-1]] = i + 1
+            values[i]  = stack[-1] + 1
+            stack.pop()
+        else:
+            print('structure error!')
+    if(len(stack) != 0 ):
+        print('structure error!')
+    # body    
+    ct = f"{adjust(len(data),6)} dG ={adjust(deltaG,10)} {tag}\n"   
+    for i in range(len(bracket)):
+        ct += f"{adjust(index[i],6)} {data[i]} {adjust(i,6)} {adjust((i+2)%(len(data)+1),6)} {adjust(values[i],6)} {adjust(index[i],7)}\n"
+    return ct
+
+
+def fasta_to_df(path):
+    with open(path, 'r') as file:
+        text = file.read()
+    lines = [line for line in text.split('\n') if len(line) > 0]
+    s = ''
+    tags = []
+    data = []
+    for l in lines:
+        if(l[0]=='>'):
+            tags.append(l)        
+            data.append(s)
+            s = ''
+        else:
+            s += l    
+    data.append(s)
+    df = pd.DataFrame(
+            {
+                'tag': tags,
+                'data': data[1:]
+            })
+    df['tag'] = df['tag'].apply(lambda x: x[1:])    
+    return df
+
+
+def df_to_fasta(df, path):
+    lines = []
+    df.apply(lambda row: lines.append(f">{row['tag']}\n{row['data']}\n"),axis=1)
+    with open(path,'w') as file:
+        file.write(''.join(lines))
+
+
+def reformat(path):
+    return path.replace('(','_').replace(')','_').replace('.','').replace(':','_')
+
+
+def reformatCT(path):
+    with open(path, 'r') as file:
+        text = file.read()
+    text = [l for l in text.split('\n') if len(l) > 0 ] # remove blank lines
+    text = '\n'.join(text)
+    text = text.replace("\t"," ")
+    while("  " in text):
+        text = text.replace("  ", " ")
+    lines = [l for l in text.split('\n')]
+    for i in range(len(lines)):
+        if(lines[i][0] == " "):
+            lines[i] = lines[i][1:]
+        if(lines[i][-1] == " "):
+            lines[i] = lines[i][:-1]
+    text = '\n'.join(lines)
+    return text
+
+
+def get_ct_data(ct):
+    ct = "\n".join(ct.split('\n')[1:])
+    df = pd.read_csv(StringIO(ct), sep=" ", header=None)               
+    nucleotide = df.iloc[:,1]
+    index = df.iloc[:,5]
+    values = df.iloc[:,4]
+    return [nucleotide, index, values]
+
+
+def ct2dot_bracket(path):
+    [nucleotide, index, values] = get_ct_data(reformatCT(path))
+    text = ''.join(nucleotide) + "\n"
+    watch = []
+    for i, v in zip(index,values):
+        if(v == 0):
+            text += '.'
+        else:
+            if( v not in watch):
+                text += '('
+                watch.append(i)
+            if( v in watch):
+                text += ')'
+    return text
+
+
+def is_nested(index, values):
+    max_value = max(index) + 10 # inf
+    for i, v in zip(index, values):
+        if(v < max_value and v != 0):
+            max_value  = v
+        if(i >= max_value):
+            max_value = max(index) + 10 # inf
+        if(v > max_value):
+            return False               
+    return True
 
 
 def get_tag_info(tag):    
@@ -1027,6 +1167,3 @@ effective_internal_loop_size_in_Hit_vicinity_regions=5, energy_calc_method="UNAF
         else:
             result[titles.msg] = "immediate branching"
     return pd.Series(result)
-
-
-df = get_row("CM040440.1|-|10048580-10049000|201-221", "./example.ct", 0)
