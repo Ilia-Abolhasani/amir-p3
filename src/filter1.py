d77a419b48b08d3e9e74b3cd9b2cf7f18a607070
maryam_hsh <maryam.hsh@gmail.com>
2022-05-19 12:23:17 +0430

    filter 1 converted to function

--- src/filter1.py
+++ src/filter1.py
@@ -1,7 +1,5 @@
-import sys
 import tqdm
 import json
-import numpy as np
 import pandas as pd
 
 
@@ -11,19 +9,20 @@ class dotdict(dict):
     __delattr__ = dict.__delitem__
 
 
-with open('./src/config/titles.json') as json_file:
-    titles = dotdict(json.load(json_file))
+def filter1_run(input_file="./Result/ct_analizer_cluster.csv", output_file="./Result/result_level1_filter.csv", chunksize=10 ** 5):
+    with open('./src/config/titles.json') as json_file:
+        titles = dotdict(json.load(json_file))
 
-header = True
-for chunk in tqdm(pd.read_csv("./Result/ct_analizer.csv", chunksize=10 ** 5)):
-    level1 = chunk[chunk[titles.msg] == '-']
-    level1 = level1[level1[titles.star_branching] != 'yes']
-    level1 = level1[level1[titles.domain_inter_struc] != 'yes']
-    level1 = level1[level1[titles.domain_star_inter_struc] != 'yes']
-    level1 = level1[level1[titles.n_term_struc] != "no residues between miR and miR*"]
+    header = True
+    for chunk in tqdm(pd.read_csv(input_file, chunksize=chunksize)):
+        level1 = chunk[chunk[titles.msg] == '-']
+        level1 = level1[level1[titles.star_branching] != 'yes']
+        level1 = level1[level1[titles.domain_inter_struc] != 'yes']
+        level1 = level1[level1[titles.domain_star_inter_struc] != 'yes']
+        level1 = level1[level1[titles.n_term_struc] != "no residues between miR and miR*"]
 
-    level1[titles.num_of_lnk_res] = level1[titles.num_of_lnk_res].apply(lambda x: int(x))
-    level1 = level1[level1[titles.num_of_lnk_res] >= 0]
+        level1[titles.num_of_lnk_res] = level1[titles.num_of_lnk_res].apply(lambda x: int(x))
+        level1 = level1[level1[titles.num_of_lnk_res] >= 0]
 
-    level1.to_csv("./Result/result_level1_filter.csv", header=header, mode='a', index=False)
-    header = False
+        level1.to_csv( output_file, header=header, mode='a', index=False)
+        header = False
